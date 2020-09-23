593bdf8344708a671ea6343b8726e5b8f68dd20c
oa2sis <95588060+oa2sis@users.noreply.github.com>
2020-09-23 10:30:27 +0000

    Fixes #90

--- app/bin/mapbox-gl-draw.js
+++ app/bin/mapbox-gl-draw.js
@@ -1051,4 +1051,45 @@ case 1:
           this.$ = yytext.replace(/\\(\\|")/g, "$"+"1")
                      .replace(/\\n/g,'\n')
                      .replace(/\\r/g,'\r')
-            
+                     .replace(/\\t/g,'\t')
+                     .replace(/\\v/g,'\v')
+                     .replace(/\\f/g,'\f')
+                     .replace(/\\b/g,'\b');
+
+break;
+case 2:
+this.$ = Number(yytext);
+break;
+case 3:
+this.$ = null;
+break;
+case 4:
+this.$ = true;
+break;
+case 5:
+this.$ = false;
+break;
+case 6:
+return this.$ = $$[$0-1];
+break;
+case 13:
+this.$ = {}; Object.defineProperty(this.$, '__line__', {
+            value: this._$.first_line,
+            enumerable: false
+        })
+break;
+case 14: case 19:
+this.$ = $$[$0-1]; Object.defineProperty(this.$, '__line__', {
+            value: this._$.first_line,
+            enumerable: false
+        })
+break;
+case 15:
+this.$ = [$$[$0-2], $$[$0]];
+break;
+case 16:
+this.$ = {}; this.$[$$[$0][0]] = $$[$0][1];
+break;
+case 17:
+
+            this.$ 
