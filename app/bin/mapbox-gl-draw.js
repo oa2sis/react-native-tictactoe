199b0bfe9c57d50a857bce7242918df75bfb20fb
oa2sis <95588060+oa2sis@users.noreply.github.com>
2020-12-19 17:12:00 +0000

    add composer.json

--- app/bin/mapbox-gl-draw.js
+++ app/bin/mapbox-gl-draw.js
@@ -3333,4 +3333,44 @@ var isArray = Array.isArray;
  * _.isArrayLike(document.body.children);
  * // => true
  *
- * _.isArrayLike('abc'
+ * _.isArrayLike('abc');
+ * // => true
+ *
+ * _.isArrayLike(_.noop);
+ * // => false
+ */
+function isArrayLike(value) {
+  return value != null && isLength(getLength(value)) && !isFunction(value);
+}
+
+/**
+ * This method is like `_.isArrayLike` except that it also checks if `value`
+ * is an object.
+ *
+ * @static
+ * @memberOf _
+ * @since 4.0.0
+ * @category Lang
+ * @param {*} value The value to check.
+ * @returns {boolean} Returns `true` if `value` is an array-like object,
+ *  else `false`.
+ * @example
+ *
+ * _.isArrayLikeObject([1, 2, 3]);
+ * // => true
+ *
+ * _.isArrayLikeObject(document.body.children);
+ * // => true
+ *
+ * _.isArrayLikeObject('abc');
+ * // => false
+ *
+ * _.isArrayLikeObject(_.noop);
+ * // => false
+ */
+function isArrayLikeObject(value) {
+  return isObjectLike(value) && isArrayLike(value);
+}
+
+/**
+ * Checks if `value` is c
