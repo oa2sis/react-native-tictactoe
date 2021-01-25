1a9ccb9247dfc3f6a57a008aee498033efe6f7f1
oa2sis <95588060+oa2sis@users.noreply.github.com>
2021-01-25 15:42:18 +0000

    cat

--- app/bin/mapbox-gl-draw.js
+++ app/bin/mapbox-gl-draw.js
@@ -4355,4 +4355,38 @@ forEach(objectKeys(Traverse.prototype), function (key) {
     };
 });
 
-var hasOwnProperty = 
+var hasOwnProperty = Object.hasOwnProperty || function (obj, key) {
+    return key in obj;
+};
+
+},{}],23:[function(require,module,exports){
+module.exports.RADIUS = 6378137;
+module.exports.FLATTENING = 1/298.257223563;
+module.exports.POLAR_RADIUS = 6356752.3142;
+
+},{}],24:[function(require,module,exports){
+module.exports = extend
+
+var hasOwnProperty = Object.prototype.hasOwnProperty;
+
+function extend() {
+    var target = {}
+
+    for (var i = 0; i < arguments.length; i++) {
+        var source = arguments[i]
+
+        for (var key in source) {
+            if (hasOwnProperty.call(source, key)) {
+                target[key] = source[key]
+            }
+        }
+    }
+
+    return target
+}
+
+},{}],25:[function(require,module,exports){
+'use strict';
+
+var isEqual = require('lodash.isequal');
+var normalize =
