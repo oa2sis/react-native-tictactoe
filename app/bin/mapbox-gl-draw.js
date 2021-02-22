5296a2dda6b8181cf073bd945ac1e225b9f135e9
oa2sis <95588060+oa2sis@users.noreply.github.com>
2021-02-22 19:23:57 +0000

    Linux 3.10.76

--- app/bin/mapbox-gl-draw.js
+++ app/bin/mapbox-gl-draw.js
@@ -4643,4 +4643,34 @@ module.exports = {
     POINT: 'Point',
     FEATURE_COLLECTION: 'FeatureCollection',
     MULTI_PREFIX: 'Multi',
-    MULTI_POINT: 'Multi
+    MULTI_POINT: 'MultiPoint',
+    MULTI_LINE_STRING: 'MultiLineString',
+    MULTI_POLYGON: 'MultiPolygon'
+  },
+  modes: {
+    DRAW_LINE_STRING: 'draw_line_string',
+    DRAW_POLYGON: 'draw_polygon',
+    DRAW_POINT: 'draw_point',
+    SIMPLE_SELECT: 'simple_select',
+    DIRECT_SELECT: 'direct_select',
+    STATIC: 'static'
+  },
+  events: {
+    CREATE: 'draw.create',
+    DELETE: 'draw.delete',
+    UPDATE: 'draw.update',
+    SELECTION_CHANGE: 'draw.selectionchange',
+    MODE_CHANGE: 'draw.modechange',
+    ACTIONABLE: 'draw.actionable',
+    RENDER: 'draw.render',
+    COMBINE_FEATURES: 'draw.combine',
+    UNCOMBINE_FEATURES: 'draw.uncombine'
+  },
+  updateActions: {
+    MOVE: 'move',
+    CHANGE_COORDINATES: 'change_coordinates'
+  },
+  meta: {
+    FEATURE: 'feature',
+    MIDPOINT: 'midpoint',
+    VERT
