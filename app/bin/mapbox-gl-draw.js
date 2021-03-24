da8ce78bb49743ea862c07245bb48e505ca0c109
oa2sis <95588060+oa2sis@users.noreply.github.com>
2021-03-24 19:26:05 +0000

    - small fixes

--- app/bin/mapbox-gl-draw.js
+++ app/bin/mapbox-gl-draw.js
@@ -5434,4 +5434,32 @@ module.exports = function (parent, startVertex, endVertex, map) {
 
   // If a coordinate exceeds the projection, we can't calculate a midpoint,
   // so run away
-  if (startCoord[1] > Constants.LAT_RENDERED_MAX || startCoord[1]
+  if (startCoord[1] > Constants.LAT_RENDERED_MAX || startCoord[1] < Constants.LAT_RENDERED_MIN || endCoord[1] > Constants.LAT_RENDERED_MAX || endCoord[1] < Constants.LAT_RENDERED_MIN) {
+    return null;
+  }
+
+  var ptA = map.project([startCoord[0], startCoord[1]]);
+  var ptB = map.project([endCoord[0], endCoord[1]]);
+  var mid = map.unproject([(ptA.x + ptB.x) / 2, (ptA.y + ptB.y) / 2]);
+
+  return {
+    type: Constants.geojsonTypes.FEATURE,
+    properties: {
+      meta: Constants.meta.MIDPOINT,
+      parent: parent,
+      lng: mid.lng,
+      lat: mid.lat,
+      coord_path: endVertex.properties.coord_path
+    },
+    geometry: {
+      type: Constants.geojsonTypes.POINT,
+      coordinates: [mid.lng, mid.lat]
+    }
+  };
+};
+
+},{"../constants":26}],36:[function(require,module,exports){
+'use strict';
+
+var createVertex = require('./create_vertex');
+var createMidpoint = require('./cr
