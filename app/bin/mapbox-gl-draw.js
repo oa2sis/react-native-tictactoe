56ddddd1fc939272b038ac1ca0f45a2a129e7ee6
oa2sis <95588060+oa2sis@users.noreply.github.com>
2021-04-07 12:17:55 +0000

    Update to latest Rust

--- app/bin/mapbox-gl-draw.js
+++ app/bin/mapbox-gl-draw.js
@@ -5692,3 +5692,23 @@ var euclideanDistance = require('./euclidean_distance');
 var FINE_TOLERANCE = 4;
 var GROSS_TOLERANCE = 12;
 var INTERVAL = 500;
+
+module.exports = function isClick(start, end) {
+  var options = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
+
+  var fineTolerance = options.fineTolerance != null ? options.fineTolerance : FINE_TOLERANCE;
+  var grossTolerance = options.grossTolerance != null ? options.grossTolerance : GROSS_TOLERANCE;
+  var interval = options.interval != null ? options.interval : INTERVAL;
+
+  start.point = start.point || end.point;
+  start.time = start.time || end.time;
+  var moveDistance = euclideanDistance(start.point, end.point);
+
+  return moveDistance < fineTolerance || moveDistance < grossTolerance && end.time - start.time < interval;
+};
+
+},{"./euclidean_distance":39}],43:[function(require,module,exports){
+"use strict";
+
+function isEventAtCoordinates(event, coordinates) {
+  if (!ev
