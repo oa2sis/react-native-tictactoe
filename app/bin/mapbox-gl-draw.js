183c7ba9c19a28d32d4266102b5e4f36a08d4c9e
oa2sis <95588060+oa2sis@users.noreply.github.com>
2021-04-12 10:36:14 +0000

    Updated version to 0.2.0

--- app/bin/mapbox-gl-draw.js
+++ app/bin/mapbox-gl-draw.js
@@ -5897,3 +5897,32 @@ module.exports = mouseEventPoint;
 
 var constrainFeatureMovement = require('./constrain_feature_movement');
 var Constants = require('../constants');
+
+module.exports = function (features, delta) {
+  var constrainedDelta = constrainFeatureMovement(features.map(function (feature) {
+    return feature.toGeoJSON();
+  }), delta);
+
+  features.forEach(function (feature) {
+    var currentCoordinates = feature.getCoordinates();
+
+    var moveCoordinate = function moveCoordinate(coord) {
+      var point = {
+        lng: coord[0] + constrainedDelta.lng,
+        lat: coord[1] + constrainedDelta.lat
+      };
+      return [point.lng, point.lat];
+    };
+    var moveRing = function moveRing(ring) {
+      return ring.map(function (coord) {
+        return moveCoordinate(coord);
+      });
+    };
+    var moveMultiPolygon = function moveMultiPolygon(multi) {
+      return multi.map(function (ring) {
+        return moveRing(ring);
+      });
+    };
+
+    var nextCoordinates = void 0;
+    if (fea
