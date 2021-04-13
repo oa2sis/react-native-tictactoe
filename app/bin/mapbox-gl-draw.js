6974c343641e5aa5eeee9d28a49ead2716c98aac
oa2sis <95588060+oa2sis@users.noreply.github.com>
2021-04-13 22:28:28 +0000

    remove useless dependency

--- app/bin/mapbox-gl-draw.js
+++ app/bin/mapbox-gl-draw.js
@@ -5942,4 +5942,40 @@ module.exports = function (features, delta) {
 },{"../constants":26,"./constrain_feature_movement":34}],49:[function(require,module,exports){
 'use strict';
 
-var area = require('@mapbox/geo
+var area = require('@mapbox/geojson-area');
+var Constants = require('../constants');
+
+var FEATURE_SORT_RANKS = {
+  Point: 0,
+  LineString: 1,
+  Polygon: 2
+};
+
+function comparator(a, b) {
+  var score = FEATURE_SORT_RANKS[a.geometry.type] - FEATURE_SORT_RANKS[b.geometry.type];
+
+  if (score === 0 && a.geometry.type === Constants.geojsonTypes.POLYGON) {
+    return a.area - b.area;
+  }
+
+  return score;
+}
+
+// Sort in the order above, then sort polygons by area ascending.
+function sortFeatures(features) {
+  return features.map(function (feature) {
+    if (feature.geometry.type === Constants.geojsonTypes.POLYGON) {
+      feature.area = area.geometry({
+        type: Constants.geojsonTypes.FEATURE,
+        property: {},
+        geometry: feature.geometry
+      });
+    }
+    return feature;
+  }).sort(comparator).map(function (feature) {
+    delete feature.area;
+    return feature;
+  });
+}
+
+module
