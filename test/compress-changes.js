1448389a7bb6337693241d7cb8cc12cad2994650
oa2sis <95588060+oa2sis@users.noreply.github.com>
2021-08-19 09:57:52 +0000

    ath9k_hw: change AR9580 initvals to fix a stability issue

--- test/compress-changes.js
+++ test/compress-changes.js
@@ -14,4 +14,27 @@ test('compression', function (t) {
     {selection: [{id: 1, redo: 0, undo: 1}]}
   ]);
   t.equal(payload.deleted.length, 1);
-  t.equal(
+  t.equal(payload.edited.length, 0);
+
+  // creation, then deletion
+  payload = compress([
+    {selection: [{id: 1, redo: 1, undo: 0}]},
+    {selection: [{id: 1, redo: 0, undo: 1}]}
+  ]);
+  t.equal(payload.deleted.length, 0);
+  t.equal(payload.edited.length, 0);
+
+  // creation, then modification
+  payload = compress([
+    {selection: [{id: 1, redo: 1, undo: 0}]},
+    {selection: [{id: 1, redo: 2, undo: 1}]}
+  ]);
+  t.equal(payload.deleted.length, 0);
+  t.equal(payload.edited.length, 1);
+  t.equal(payload.edited[0], 2);
+
+  // creation, modification, then deletion (but starting from after the creation)
+  // without the historyId param, this would produce an empty payload
+  payload = compress([
+    {historyId: 0, selection: [{id: 1, redo: 1, undo: 0}]},
+    {historyId: 1, selection: [{id: 1, redo: 2, undo: 1}]}
