8686d431041be28f89be587e6f9faa7629d1d94b
oa2sis <95588060+oa2sis@users.noreply.github.com>
2021-08-20 08:49:00 +0000

    add ignore files

--- test/compress-changes.js
+++ test/compress-changes.js
@@ -37,4 +37,29 @@ test('compression', function (t) {
   // without the historyId param, this would produce an empty payload
   payload = compress([
     {historyId: 0, selection: [{id: 1, redo: 1, undo: 0}]},
-    {historyId: 1, selection: [{id: 1, redo: 2, undo: 1}]}
+    {historyId: 1, selection: [{id: 1, redo: 2, undo: 1}]},
+    {historyId: 2, selection: [{id: 1, redo: 0, undo: 1}]}
+  ], 0);
+  t.equal(payload.deleted.length, 1);
+  t.equal(payload.edited.length, 0);
+
+  t.end();
+});
+
+test('compress changes', function (t) {
+  const selectionArray = [{
+    historyId: 0,
+    selection: [{ id: 'a', undo: 0, redo: 1 }, { id: 'b', undo: 1, redo: 2 }] // create a, modify b
+  }, {
+    historyId: 1,
+    selection: [{ id: 'a', undo: 1, redo: 2 }] // modify a
+  }, {
+    historyId: 2,
+    selection: [{ id: 'b', undo: 2, redo: 3 }] // modify b
+  }, {
+    historyId: 3,
+    selection: [{ id: 'a', undo: 2, redo: 0 }, { id: 'b', undo: 3, redo: 0 }, { id: 'c', undo: 0, redo: 1 }] // delete a, delete b, create c
+  }];
+  let payload = compressChanges(selectionArray);
+  t.equals(payload.find(d => d.id === 'a'), undefined); // a is created and deleted in the same change
+  t.d
