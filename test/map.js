c6a69227fc53d43c4a9cc24a57dd50bf61533094
oa2sis <95588060+oa2sis@users.noreply.github.com>
2021-08-21 21:25:39 +0000

    more javadocs

--- test/map.js
+++ test/map.js
@@ -9,4 +9,32 @@ App.glSupport = true;
 
 // stub global DOM methods
 import jsdom from 'jsdom';
-const doc = jsdom.jsdom('<!doctype html><html><
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = doc.defaultView;
+global.document = doc;
+
+// stub mapbox draw
+const draw = () => true;
+draw.prototype.add = () => true;
+draw.prototype.onAdd = () => true;
+draw.prototype.getSelected = () => ({ features: [{ properties: {} }] });
+draw.prototype.getSelectedPoints = () => ({ features: [{ properties: {} }] });
+draw.prototype.getMode = () => 'simple_select';
+draw.prototype.changeMode = () => true;
+
+const { Map } = proxyquire.noCallThru().load('../app/scripts/components/map', {
+  '@mapbox/mapbox-gl-draw': draw,
+  'mapbox-gl': mock,
+  '../../util/app': App
+});
+
+function setup (options) {
+  options = options || {};
+  const props = Object.assign({
+    selection: {
+      past: [],
+      present: { historyId: 'initial' },
+      future: []
+    },
+    map: {
+     
